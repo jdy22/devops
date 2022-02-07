a073773092d0eb34cf872a8969139dd962e243b1
Ivan Procaccini <ivanprocaccini905@gmail.com>
2022-02-07 14:10:26 +0000

    Add Gunicorn entrypoint

---
+++ wsgi.py
@@ -0,0 +1,9 @@
+from literature_searcher import create_app
+
+############################################
+# GUNICORN WEBSERVER ENTRY POINT  |  |  |  #
+############################################
+
+app = create_app()
+
+############################################
