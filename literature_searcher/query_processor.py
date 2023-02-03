a3e03cebc2cc8cc9bd5456de7eb1eaae9ae61321
Jo Ye <jdy15@ic.ac.uk>
2023-02-03 08:06:02 +0000

    Removed bad commit

--- literature_searcher/query_processor.py
+++ literature_searcher/query_processor.py
@@ -1,8 +1,8 @@
 database = {
-    # "shakespeare": """William Shakespeare (26 April 1564 - 23 April 1616) was an
-    #    English poet, playwright, and actor, widely regarded as the
-    #    greatest writer in the English language and the world's
-    #    pre-eminent dramatist.""",
+     "shakespeare": """William Shakespeare (26 April 1564 - 23 April 1616) was an
+        English poet, playwright, and actor, widely regarded as the
+        greatest writer in the English language and the world's
+        pre-eminent dramatist.""",
     "asimov": """Isaac Asimov (2 January 1920 - 6 April 1992) was an
         American writer and professor of Biochemistry, famous for
         his works of hard science fiction and popular science.""",
